7afc0a969b04438f037d9a8535e2d166124c7408
Bassem Yacoube <bassemfg@gmx.net>
2018-06-11 18:31:12 -0700

    q

--- WQU Py2 MP1.py
+++ WQU Py2 MP1.py
@@ -6,6 +6,8 @@ import numpy as np_data
 from io import StringIO
 import csv
 from scipy.interpolate import interp1d
+from scipy.optimize import leastsq
+import numpy.polynomial.polynomial as poly
 
 quandl.ApiConfig.api_key ='RqpDsLqAEJyHa7iAADNQ'
 
@@ -48,64 +50,46 @@ plt.show()
 
 #except Exception as e:
 #   print( e)
-    
-   # data provided
-   x=np.array([1.0,2.5,3.5,4.0,1.1,1.8,2.2,3.7])
-   y=np.array([6.008,15.722,27.130,33.772,5.257,9.549,11.098,28.828])
-   # here, create lambda functions for Line, Quadratic fit
-   # tpl is a tuple that contains the parameters of the fit
-   funcLine=lambda tpl,x : tpl[0]*x+tpl[1]
-   funcQuad=lambda tpl,x : tpl[0]*x**2+tpl[1]*x+tpl[2]
-   # func is going to be a placeholder for funcLine,funcQuad or whatever 
-   # function we would like to fit
-   func=funcLine
-   # ErrorFunc is the diference between the func and the y "experimental" data
-   ErrorFunc=lambda tpl,x,y: func(tpl,x)-y
-   #tplInitial contains the "first guess" of the parameters 
-   tplInitial1=(1.0,2.0)
-   # leastsq finds the set of parameters in the tuple tpl that minimizes
-   # ErrorFunc=yfit-yExperimental
-   tplFinal1,success=leastsq(ErrorFunc,tplInitial1[:],args=(x,y))
-   print " linear fit ",tplFinal1
-   xx1=np.linspace(x.min(),x.max(),50)
-   yy1=func(tplFinal1,xx1)
-   #------------------------------------------------
-   # now the quadratic fit
-   #-------------------------------------------------
-   func=funcQuad
-   tplInitial2=(1.0,2.0,3.0)
 
-   tplFinal2,success=leastsq(ErrorFunc,tplInitial2[:],args=(x,y))
-   print "quadratic fit" ,tplFinal2
-   xx2=xx1
+x_sqrd = [i**2 for i in x0]
+# here, create lambda functions for Line, Quadratic fit
+# tpl is a tuple that contains the parameters of the fit
+funcLine=lambda tpl,x0 : tpl[0]*x0+tpl[1]
+# func is going to be a placeholder for funcLine,funcQuad or whatever 
+# function we would like to fit
+func=funcLine
+# ErrorFunc is the diference between the func and the y "experimental" data
+ErrorFunc=lambda tpl,x0,y0: func(tpl,x0)-y0
+#tplInitial contains the "first guess" of the parameters 
+tplInitial1=(1.0,2.0)
+# leastsq finds the set of parameters in the tuple tpl that minimizes
+# ErrorFunc=yfit-yExperimental
+tplFinal1,success=leastsq(ErrorFunc,tplInitial1[:],args=(x0,y0))
+print (" linear fit ",tplFinal1)
+xx1=np.linspace(0, len(data_array)-1,len(data_array))
+yy1=func(tplFinal1,xx1)
+#------------------------------------------------
+# now the quadratic fit
+#-------------------------------------------------
+funcQuad=lambda tpl,x0 : np.float64(tpl[0])*np.float64(x_sqrd)+np.float64(tpl[1])*np.float64(x0)+np.float64(tpl[2])
+func=funcQuad
+tplInitial2=(1.0,2.0,3.0)
 
-   yy2=func(tplFinal2,xx2)
-   plt.plot(xx1,yy1,'r-',x,y,'bo',xx2,yy2,'g-')
-   plt.show()
-   
-from numpy import random , histogram , arange , sqrt , exp , nonzero, array
-from scipy.optimize import leastsq
-import pylab
-if __name__ == '__main__':
-n = 1000; isi=random.exponential(0.1 , size=n)
-db = 0.01; bins = arange (0 ,1.0 , db)
-h = histogram(isi,bins)[0]
-p = h.astype(float)/n/db
-# Function to be fit
-# x: independent variable
-# p: tuple of parameters
-fitfunc = lambda p,x:exp(-x/p[0])/p[0]
-# Standard form, here err is absolute error
-errfunc= lambda p, x , y , err : (y - fitfunc (p, x))/err
-# Initial values for fit parameters
-initialFitP = array([0.2])
-# Hist count less than four has poor estimate of the weight
-# Don't use in the fitting process
-idx = nonzero(h>4)
-out = leastsq( errfunc, initialFitP, args=( bins[idx] +0.01/2 , p[idx] ,p[idx] /sqrt(h[idx])))
-l1 = 'Actual Data Points'
-pylab.errorbar( bins[idx],p[idx], yerr=p[idx]/sqrt(h[idx]), fmt='ko', label=l1, color='red' )
-l2 = 'Best Fit'
-pylab.plot(bins,fitfunc((out[0],),bins),'b--',lw=2,label=l2 )
-pylab.legend()
-pylab.show ()
+tplFinal2,success=leastsq(ErrorFunc,tplInitial2[:],args=(x0,y0))
+print ("quadratic fit" ,tplFinal2)
+xx2=np.array(xx1)
+
+yy2=func(tplFinal2,xx2)
+plt.plot(xx1,yy1,'r-',label='linear')
+plt.plot(x0,y0,'bo')
+plt.plot(xx2,yy2,'g-',label='quadratic')
+plt.legend()
+plt.show()
+
+#using polyfit
+coefs = poly.polyfit(x0, y0, 2)
+ffit = poly.polyval(xx2, coefs)
+plt.plot(x0,y0,'bo')
+plt.plot(xx2, ffit,'g', label='Polyfit quadratic')
+plt.legend()
+plt.show()
